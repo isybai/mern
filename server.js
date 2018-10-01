7b4b87fd921c6744067916a311bc919dc2af7231
islambek toychuev <islambek.toichuev@zensoft.io>
2018-10-01 11:46:09 +0600

    Profile: Get Update Create

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 
 const users = require('./routes/api/users');
-const profiles = require('./routes/api/profiles');
+const profiles = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
