51314ae8259c820500a1d57b9088546d5d0581ec
islambek toychuev <islambek.toichuev@zensoft.io>
2018-10-01 11:57:18 +0600

    code refactoring

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -40,26 +40,27 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
   // Get fields
   const profileFields = {};
   profileFields.user = req.user.id;
-  if(req.body.handle) profileFields.handle = req.body.handle;
-  if(req.body.company) profileFields.company = req.body.company;
-  if(req.body.website) profileFields.website = req.body.website;
-  if(req.body.location) profileFields.location = req.body.location;
-  if(req.body.bio) profileFields.bio = req.body.bio;
-  if(req.body.status) profileFields.status = req.body.status;
+  if(req.body.handle) profileFields.handle      = req.body.handle;
+  if(req.body.company) profileFields.company    = req.body.company;
+  if(req.body.website) profileFields.website    = req.body.website;
+  if(req.body.location) profileFields.location  = req.body.location;
+  if(req.body.bio) profileFields.bio            = req.body.bio;
+  if(req.body.status) profileFields.status      = req.body.status;
   if(req.body.githubUsername) profileFields.githubUsername = req.body.githubUsername;
+
+  // Social
+  profileFields.social = {};
+  if(req.body.youtube) profileFields.social.youtube     = req.body.youtube;
+  if(req.body.twitter) profileFields.social.twitter     = req.body.twitter;
+  if(req.body.facebook) profileFields.social.facebook   = req.body.facebook;
+  if(req.body.linkedin) profileFields.social.linkedin   = req.body.linkedin;
+  if(req.body.instagram) profileFields.social.instagram = req.body.instagram;
 
   // Skills - Split into array
   if(typeof req.body.skills !== 'undefined') {
     profileFields.skills = req.body.skills.split(',');
   }
 
-  // Social
-  profileFields.social = {};
-  if(req.body.youtube) profileFields.social.youtube = req.body.youtube;
-  if(req.body.twitter) profileFields.social.twitter = req.body.twitter;
-  if(req.body.facebook) profileFields.social.facebook = req.body.facebook;
-  if(req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
-  if(req.body.instagram) profileFields.social.instagram = req.body.instagram;
 
   Profile.findOne({ user: req.user.id })
     .then(profile => {
